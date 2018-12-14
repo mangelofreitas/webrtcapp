7c3d615352da8bcbadb22cd89a8be7336e691ae9
Miguel Ângelo Simões Freitas <mfreitas@student.dei.uc.pt>
2018-12-14 03:05:29 +0000

    Change turn server

--- WebRTCApp/controllers/call.js
+++ WebRTCApp/controllers/call.js
@@ -108,9 +108,9 @@
                     iceServers: [
                         {urls:'stun:stun.l.google.com:19302'},
                         {
-                            urls: 'turn:numb.viagenie.ca',
-                            credential: 'muazkh',
-                            username: 'webrtc@live.com'
+                            urls: 'turn:192.158.29.39:3478?transport=udp',
+                            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                            username: '28224511:1379330808'
                         }
                     ]
                 };
